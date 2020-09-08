e3d631c28f564e29e663c2f80f5421755478e178
Christian Jensen <christian@sinch.com>
2020-09-08 12:16:22 -0700

    bump of verstion, fixed remote links validation

--- src/validators.js
+++ src/validators.js
@@ -1,197 +1,229 @@
-const request = require('request-promise-native');
-const path = require('path');
-const { fileExists } = require('./tools');
-const { Page, Heading, XrefLink, UrlLink, MailtoLink, Image } = require('./catalog');
-
+const request = require("request-promise-native");
+const path = require("path");
+const { fileExists } = require("./tools");
+const {
+  Page,
+  Heading,
+  XrefLink,
+  UrlLink,
+  MailtoLink,
+  Image,
+} = require("./catalog");
 
 class Validator {
-    /**
-     * Runs one or multiple validations on the page provided, and returns a Promise. That promise will not resolve to
-     * any significant value, they'll just act as a marker of completion.
-     * @param catalog the catalog instance we're validating
-     * @param page a specific page to apply the validations on.
-     * @param options The current options as provided to the CLI.
-     * @param errorCallback A function that will be called for each validation error, with signature (Element, String),
-     * where the 2nd argument is the error message for the element in question.
-     */
-    validate(catalog, page, options, errorCallback) {
-        throw new Error("Abstract method not implemented");
-    }
+  /**
+   * Runs one or multiple validations on the page provided, and returns a Promise. That promise will not resolve to
+   * any significant value, they'll just act as a marker of completion.
+   * @param catalog the catalog instance we're validating
+   * @param page a specific page to apply the validations on.
+   * @param options The current options as provided to the CLI.
+   * @param errorCallback A function that will be called for each validation error, with signature (Element, String),
+   * where the 2nd argument is the error message for the element in question.
+   */
+  validate(catalog, page, options, errorCallback) {
+    throw new Error("Abstract method not implemented");
+  }
 }
 
-
 class ElementValidator extends Validator {
-    constructor(filterFn) {
-        super();
-        this.filterFn = filterFn;
-    }
+  constructor(filterFn) {
+    super();
+    this.filterFn = filterFn;
+  }
 
-    async validate(catalog, page, options, errorCallback) {
-        return Promise.all(
-            page.elements
-                .filter(this.filterFn)
-                .map(link => this.resolve(link, catalog, options).catch(err => errorCallback(link, err)))
-        );
-    }
+  async validate(catalog, page, options, errorCallback) {
+    return Promise.all(
+      page.elements
+        .filter(this.filterFn)
+        .map((link) =>
+          this.resolve(link, catalog, options).catch((err) =>
+            errorCallback(link, err)
+          )
+        )
+    );
+  }
 }
 
 class XrefLinkValidator extends ElementValidator {
-    constructor() {
-        super(link => link instanceof XrefLink);
-    }
+  constructor() {
+    super((link) => link instanceof XrefLink);
+  }
 
-    async resolve(link, catalog) {
-        return new Promise((resolve, reject) => {
-            const {slug, anchor} = link;
+  async resolve(link, catalog) {
+    return new Promise((resolve, reject) => {
+      const { slug, anchor } = link;
 
-            if (!slug && !anchor) {
-                reject(`Invalid xref format`);
-                return;
-            }
+      if (!slug && !anchor) {
+        reject(`Invalid xref format`);
+        return;
+      }
 
-            const page = slug ? catalog.find(Page.bySlug(slug)) : link.page;
-            
-            let target = page;
+      const page = slug ? catalog.find(Page.bySlug(slug)) : link.page;
 
-            if (target === undefined) {
-                reject(`Xref does not resolve to a known internal page.`);
-                return;
-            }
+      let target = page;
 
-            if (anchor !== undefined) {
+      if (target === undefined) {
+        reject(`Xref does not resolve to a known internal page.`);
+        return;
+      }
 
-                target = page.findElement(Heading.bySlug(anchor));
-            }
+      if (anchor !== undefined) {
+        target = page.findElement(Heading.bySlug(anchor));
+      }
 
-            if (target === undefined) {
-                let suggestion = `${anchor}`;
-                if (page.findElement(Heading.bySlug(suggestion))) {
-                    reject(`Section '${anchor}' wasn't found but a section with a similar slug was found. Did you mean '#${suggestion}'?`);
-                } else {
-                    reject(`Xref resolves to a known page but section could not be found.`);
-                }
-                return;
-            }
+      if (target === undefined) {
+        let suggestion = `${anchor}`;
+        if (page.findElement(Heading.bySlug(suggestion))) {
+          reject(
+            `Section '${anchor}' wasn't found but a section with a similar slug was found. Did you mean '#${suggestion}'?`
+          );
+        } else {
+          reject(
+            `Xref resolves to a known page but section could not be found.`
+          );
+        }
+        return;
+      }
 
-            resolve(target);
-        });
-    }
+      resolve(target);
+    });
+  }
 }
 
 class MailtoLinkValidator extends ElementValidator {
-    constructor() {
-        super(link => link instanceof MailtoLink);
-    }
+  constructor() {
+    super((link) => link instanceof MailtoLink);
+  }
 
-    async resolve(link) {
-        return new Promise((resolve, reject) => {
-            if (!link.href.startsWith('mailto:')) {
-                reject('Email links should start with mailto:');
-                return;
-            }
-            resolve(this.href);
-        });
-    }
+  async resolve(link) {
+    return new Promise((resolve, reject) => {
+      if (!link.href.startsWith("mailto:")) {
+        reject("Email links should start with mailto:");
+        return;
+      }
+      resolve(this.href);
+    });
+  }
 }
 
 class HrefValidator extends ElementValidator {
+  async resolve(link, catalog, options) {
+    if (link.page.hidden) return;
+    if (link.isLocal()) {
+      return this.resolveLocal(...arguments);
+    } else if (link.isRemote() && link.href.startsWith("http")) {
+      return this.resolveRemote(...arguments);
+    }
+  }
 
-    async resolve(link, catalog, options) {
-        if (link.isLocal()) {
-            return this.resolveLocal(...arguments);
-        } else if (link.isRemote()) {
-            return this.resolveRemote(...arguments);
-        }
+  async resolveLocal(link, catalog, options) {
+    const page = link.page;
+
+    var filePath = link.href.split("?")[0]; // relative paths can include '?' as query strings, only keep path
+    filePath = filePath.replace(/\\/g, "/");
+    const fileLocation = path.normalize(
+      path.resolve(
+        options.dir,
+        path.normalize(page.directory),
+        path.normalize(filePath)
+      )
+    );
+    return fileExists(fileLocation).catch(() => {
+      console.log(options.dir);
+      console.log(page.directory);
+      console.log(filePath);
+      console.log(link.markdown);
+      reject("Local file1: " + fileLocation + " does not exist.");
+      return;
+    });
+  }
+
+  async resolveRemote(link) {
+    const href = link.href;
+
+    function checkError(err) {
+      console.log(link.page.hidden);
+      if (err.statusCode == undefined) {
+        throw "Link is invalid or domain doesnt exist:" + link.href;
+      }
+      if (err.statusCode.startsWith("4")) {
+        throw link.href + ` URL seems broken: HTTP Status ${err.statusCode}`;
+      }
+      if (err.statusCode.startsWith("5")) {
+        throw link.href + ` URL seems broken: HTTP Status ${err.statusCode}`;
+      }
+      if (err.statusCode.startsWith("3")) {
+        throw link.href + ` URL seems broken: HTTP Status ${err.statusCode}`;
+      }
     }
 
-    async resolveLocal(link, catalog, options) {
-        const page = link.page;
-     
-        var filePath = link.href.split('?')[0];  // relative paths can include '?' as query strings, only keep path
-        filePath = filePath.replace(/\\/g, "/");
-        const fileLocation = path.normalize(path.resolve(options.dir, path.normalize(page.directory), path.normalize(filePath)));
-        //const fileLocation =  path.normalize(filePath)
-        return fileExists(fileLocation)
-            .catch(() => { 
-                console.log(options.dir);
-                console.log(page.directory);
-                console.log(filePath);
-                console.log(link.markdown);
-                throw `Local file1: ${fileLocation} does not exist.` });
+    async function attemptFetch(httpOp) {
+      return httpOp(href, {
+        headers: {
+          Accept: "*/*",
+          "User-Agent": "curl/7.54.0", // some servers block requests from scripts, attempt cURL impersonation
+        },
+        timeout: 5000, // ms
+        followRedirect: true,
+      });
     }
 
-    async resolveRemote(link) {
-        const href = link.href;
-
-        function checkError(err) {
-            if ([400, 404, 401, 500].includes(err.statusCode)) {
-                throw `URL seems broken: HTTP Status ${err.statusCode}`;
-            }
+    // first attempt a HEAD operation
+    return attemptFetch(request.head)
+      .catch((err) => {
+        // if HEAD fails with 404, retry with GET
+        if (err.statusCode === 404) {
+          return attemptFetch(request.get);
         }
-
-        async function attemptFetch(httpOp) {
-            return httpOp(href, {
-                headers: {
-                    'Accept': '*/*',
-                    'User-Agent': 'curl/7.54.0',  // some servers block requests from scripts, attempt cURL impersonation
-                }
-            });
-        }
-
-        // first attempt a HEAD operation
-        return attemptFetch(request.head)
-            .catch(err => {
-                // if HEAD fails with 404, retry with GET
-                if (err.statusCode === 404) {
-                    return attemptFetch(request.get);
-                }
-                checkError(err);
-            }).catch(err => {
-                checkError(err);
-                return href;
-            })
-            .then(resp => href);
-    }
+        checkError(err);
+      })
+      .catch((err) => {
+        checkError(err);
+        return href;
+      })
+      .then((resp) => href);
+  }
 }
 
-
 class HeadingsValidator extends Validator {
-
-    async validate(catalog, page, options, errorCallback) {
-        const headings = page.headings.filter(heading => heading.depth === 1);
-        headings.forEach(heading => errorCallback(heading,
-            'Heading with level 1 are reserved for page titles. ' +
-            'Use headings of level 2 and more in content files.'));
-    }
+  async validate(catalog, page, options, errorCallback) {
+    const headings = page.headings.filter((heading) => heading.depth === 1);
+    headings.forEach((heading) =>
+      errorCallback(
+        heading,
+        "Heading with level 1 are reserved for page titles. " +
+          "Use headings of level 2 and more in content files."
+      )
+    );
+  }
 }
 
 class WhatsNextValidator extends Validator {
-
-    async validate(catalog, page, options, errorCallback) {
-        const next = page.headers.next;
-        if (next && next.pages) {
-            for (const slug of next.pages) {
-                if (catalog.find(Page.bySlug(slug)) === undefined) {
-                    const element = {
-                        ref: page.ref,
-                        desc: slug
-                    };
-                    errorCallback(element,
-                        'Invalid page reference in `next.pages` front matter entry. ' +
-                        'A page with this slug could not be found in local catalog.');
-                }
-            }
+  async validate(catalog, page, options, errorCallback) {
+    const next = page.headers.next;
+    if (next && next.pages) {
+      for (const slug of next.pages) {
+        if (catalog.find(Page.bySlug(slug)) === undefined) {
+          const element = {
+            ref: page.ref,
+            desc: slug,
+          };
+          errorCallback(
+            element,
+            "Invalid page reference in `next.pages` front matter entry. " +
+              "A page with this slug could not be found in local catalog."
+          );
         }
+      }
     }
+  }
 }
 
-
 module.exports = {
-    xrefs: new XrefLinkValidator(),
-    urls: new HrefValidator(link => link instanceof UrlLink),
-    mailtos: new MailtoLinkValidator(),
-    images: new HrefValidator(link => link instanceof Image),
-    headings: new HeadingsValidator(),
-    whatsnext: new WhatsNextValidator(),
+  xrefs: new XrefLinkValidator(),
+  urls: new HrefValidator((link) => link instanceof UrlLink),
+  mailtos: new MailtoLinkValidator(),
+  images: new HrefValidator((link) => link instanceof Image),
+  headings: new HeadingsValidator(),
+  whatsnext: new WhatsNextValidator(),
 };
-
