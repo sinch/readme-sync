f39b941cfcde05764fdb681140f9ef9fb302a0e8
Amar Vrbanjac <amarvrbanjac@gmail.com>
2020-11-06 13:37:03 +0100

    fix yamlexption and promise handling exception

--- src/catalog.js
+++ src/catalog.js
@@ -191,7 +191,7 @@ class Page {
       }
     }
 
-    return `---\n${yaml.safeDump(frontMatter)}---\n${this.content}`;
+    return `---\n${yaml.safeDump(frontMatter, {skipInvalid: true})}---\n${this.content}`;
   }
 
   async writeTo(baseDir) {
@@ -203,6 +203,8 @@ class Page {
         if (err) reject(err);
         else resolve(outputFile);
       });
+    }).catch(err => {
+      console.error(err);
     });
   }
 
